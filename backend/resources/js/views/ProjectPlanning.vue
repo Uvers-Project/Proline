fb2325f07d643ad5c28cc7ee1926adf2e0883f3f
Yuriko Aishinselo <aishinseloyuriko124@gmail.com>
2026-06-30 14:11:11 +0700

    Remove add real plan in add task modal

--- backend/resources/js/views/ProjectPlanning.vue
+++ backend/resources/js/views/ProjectPlanning.vue
@@ -176,16 +176,7 @@
             </div>
           </div>
           
-          <div class="grid grid-cols-2 gap-4">
-            <div>
-               <label class="block text-sm font-medium text-gray-700 mb-1">Real Start Date</label>
-               <input type="date" v-model="newTask.real_start_date" :class="{'border-red-400 ring-2 ring-red-400/20': formErrors.real_start_date}" class="w-full px-4 py-2 border border-gray-200 rounded-xl focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 outline-none transition-colors bg-blue-50/50" />
-            </div>
-            <div>
-               <label class="block text-sm font-medium text-gray-700 mb-1">Real End Date</label>
-               <input type="date" v-model="newTask.real_end_date" :class="{'border-red-400 ring-2 ring-red-400/20': formErrors.real_end_date}" class="w-full px-4 py-2 border border-gray-200 rounded-xl focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 outline-none transition-colors bg-blue-50/50" />
-            </div>
-          </div>
+
 
 
           <div class="grid grid-cols-2 gap-4">
